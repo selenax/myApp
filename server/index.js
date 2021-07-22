8481c805df9b4e3cf2e9f7e921b8508840a11050
selenax <selenawenxu@gmail.com>
2021-07-21 23:12:15 -0700

    deployed heroku success

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({message: "testing 123"})
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
