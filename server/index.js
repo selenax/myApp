5df5ad34f6d24f5a4d3a12c07aa60cd4d98cf41a
selenax <selenawenxu@gmail.com>
2021-07-24 23:17:41 -0700

    Adding mongoDB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-// dynamic port binding for heroku
-const PORT = process.env.PORT || 5000;
+mongoose.connect();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({message: "testing 123"})
 })
+
+// dynamic port binding for heroku
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
